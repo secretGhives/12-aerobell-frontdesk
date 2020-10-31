6f443c34e81f799c953dc03cc3bdd821ffcafbb4
Hooker Juggernaut <secretgspot@gmail.com>
2020-10-31 01:07:24 -0600

    arihive

---
+++ FrontDesk/assets/php/metar.php
@@ -0,0 +1,27 @@
+<?php
+
+$config['icao']       = $_GET['icao']; // url of html to grab
+$location = $_GET['icao'];
+get_metar($location);
+
+function get_metar($location) {
+$fileName = "http://weather.noaa.gov/pub/data/observations/metar/stations/$location.TXT";
+//$fileName = "http://weather.noaa.gov/pub/data/observations/metar/decoded/$location.TXT";
+	$metar = '';
+	$fileData = @file($fileName) or die('METAR not available');
+	if ($fileData != false) {
+		list($i, $date) = each($fileData);
+
+		$utc = strtotime(trim($date));
+		$time = date("D, F jS Y g:i A",$utc);
+
+		while (list($i, $line) = each($fileData)) {
+			$metar .= ' ' . trim($line);
+			}
+		$metar = trim(str_replace('  ', ' ', $metar));
+		}
+	//echo "METAR FOR $location (Issued: $time UTC):<br>$metar";
+	echo "$metar";
+	}
+
+?>
---
+++ Prototype/README.md
@@ -0,0 +1,11 @@
+.__.         .__ .___.   .   
+[__] _ ._. _ [__)[__ |   |   
+|  |(/,[  (_)[__)[___|___|___
+--------------------------------
+  Multi-Modular System
+
+  #1 Reservations
+  	- Flight board / Weather station
+  	- Flight dispatch 
+
+  
